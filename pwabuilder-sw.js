f65a2e31e517776dac86890c090e4eef8248d413
englishextra <englishextra@yandex.ru>
2018-09-06 21:51:21 +0300

    updates

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,3 +1,6 @@
+/*!
+ * @see {@link https://github.com/pwa-builder/serviceworkers/pull/28}
+ */
 //This is the "Offline copy of pages" service worker
 
 //Install stage sets up the index page (home page) in the cache and opens a new cache
@@ -6,7 +9,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+        console.log('[PWA Builder] Cached index page during Install: '+ response.url);
         return cache.put(indexPage, response);
       });
   }));
@@ -17,7 +20,7 @@ self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
     return caches.open('pwabuilder-offline').then(function (cache) {
       return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
+        console.log('[PWA Builder] add page to offline: '+response.url)
         return cache.put(request, response);
       });
     });

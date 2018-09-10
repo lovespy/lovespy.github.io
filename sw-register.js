f2e2b61f3be7871000fc6075f180611f0691d96f
englishextra <englishextra@yandex.ru>
2018-09-10 13:02:41 +0300

    updates

--- sw-register.js
+++ sw-register.js
@@ -1,13 +1,17 @@
 //This is the "Offline copy of pages" service worker
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-if (navigator.serviceWorker.controller) {
-	console.log("[sw-register.js] Active service worker found, no need to register.");
-} else {
-	//Register the ServiceWorker
-	navigator.serviceWorker.register("./sw.js", {
-		scope: "./"
-	}).then(function(reg) {
-		console.log("[sw-register.js] Service worker has been registered for scope: " + reg.scope);
-	});
+var swJs = document.currentScript && document.currentScript.dataset.serviceWorker;
+if (swJs) {
+	if (navigator.serviceWorker.controller) {
+		console.log("[sw-register.js] Active service worker found, no need to register.");
+	} else {
+		//Register the ServiceWorker
+		navigator.serviceWorker.register(swJs, {
+			scope: "./"
+		}).then(function(reg) {
+			console.log("[sw-register.js] Service worker has been registered for scope: " + reg.scope);
+		});
+	}
 }
+

728a59dc93a4cc813d1e255f1c254ad879b36a61
englishextra <englishextra@yandex.ru>
2018-09-11 20:31:05 +0300

    updates

--- libs/lovespy-muicss/src/bundle.js
+++ libs/lovespy-muicss/src/bundle.js
@@ -2400,7 +2400,7 @@ unescape, VK, WheelIndicator, Ya*/
 									clearTimeout(timer);
 									timer = null;
 									scroll2Top(0, 20000);
-								}, 500);
+								}, 1000);
 						}
 					};
 					if (img && !macyContainer[classList].contains(isActiveClass)) {
